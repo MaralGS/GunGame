03fc08838f278204093a1df3d463556f1a214fa0
Pol Maresch <polmaresch@gmail.com>
2023-10-15 14:10:48 +0200

    try server

--- Assets/TCP_Client.cs
+++ Assets/TCP_Client.cs
@@ -16,12 +16,14 @@ using System.Text;
 
 public class TCP_Client : MonoBehaviour
 {
-    public static void Start()
+    byte[] data = new byte[1024];
+    [SerializeField] string ip = "127.0.0.1";
+   void Start()
     {
-        byte[] data = new byte[1024];
+    
         string input, stringData;
         IPEndPoint ipep = new IPEndPoint(
-                        IPAddress.Parse("127.0.0.1"), 9050);
+                        IPAddress.Parse(ip), 9050);
 
         Socket server = new Socket(AddressFamily.InterNetwork,
                        SocketType.Stream, ProtocolType.Tcp);
@@ -42,17 +44,15 @@ public class TCP_Client : MonoBehaviour
         stringData = Encoding.ASCII.GetString(data, 0, recv);
         Console.WriteLine(stringData);
 
-        while (true)
-        {
-            input = Console.ReadLine();
-            if (input == "exit")
-                break;
-            server.Send(Encoding.ASCII.GetBytes(input));
-            data = new byte[1024];
-            recv = server.Receive(data);
-            stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine(stringData);
-        }
+    
+       input = Console.ReadLine();
+
+       server.Send(Encoding.ASCII.GetBytes(input));
+       data = new byte[1024];
+       recv = server.Receive(data);
+       stringData = Encoding.ASCII.GetString(data, 0, recv);
+       Console.WriteLine(stringData);
+ 
         Console.WriteLine("Disconnecting from server...");
         server.Shutdown(SocketShutdown.Both);
         server.Close();
