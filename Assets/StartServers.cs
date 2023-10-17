a8eb14dc57dc1f233d18abe83fc7fc9af7ce791d
Pol Maresch <polmaresch@gmail.com>
2023-10-17 12:02:35 +0200

    UI MAKED

---
+++ Assets/StartServers.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartServers : MonoBehaviour
+{
+    public GameObject TCP;
+    public GameObject UDP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
