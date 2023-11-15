bed4eae17f67669346d720dd3e50f113c52c15f5
Pau Olmos Serrano <79161085+PauOlmos@users.noreply.github.com>
2023-11-15 12:09:00 +0100

    Player Jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,139 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovment : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float verticalInput;
+    public float movementSpeed;
+    private float speed = 0.0f;
+    public float jumpSpeed = 0.0f;
+    public float maxSpeed = 1.0f;
+    public float maxJumpTime = 3.0f;
+    private bool isGrounded = false;
+    public float jumpForce = 10.0f;
+    public Quaternion q;
+    public float jumpTimeCounter = 0.0f;
+    public float minJumpForce;
+    public float jumpTime;
+    public float maxJumpForce;
+
     void Start()
     {
-        
+        movementSpeed = movementSpeed / 30;
+        q = new Quaternion((float)0.707106829, (float)0.707106829, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+    
+        if (Input.GetKey(KeyCode.A))
+        {
+           speed += movementSpeed * Time.deltaTime;
+            if(speed > maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+        }
+        else if(speed > 0)
+        {
+            speed += -movementSpeed * Time.deltaTime;
+            if(speed < 0)
+            {
+                speed = 0;
+            }
+        }
         
+        if (Input.GetKey(KeyCode.D))
+        {
+           speed -= movementSpeed * Time.deltaTime;
+            if(speed < -maxSpeed)
+            {
+                speed = -maxSpeed;
+            }
+        }
+        else if(speed < 0)
+        {
+            speed += movementSpeed * Time.deltaTime;
+            if(speed > 0)
+            {
+                speed = 0;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            StartJump();
+        }
+
+        if (Input.GetButton("Jump") && jumpTimeCounter > 0 && isGrounded)
+        {
+            ContinueJump();
+        }
+
+        if (Input.GetButtonUp("Jump") && isGrounded)
+        {
+            EndJump();
+        }
+
+        if (jumpTimeCounter < 0 && isGrounded)
+        {
+            EndJump();
+        }
+
+        transform.position += Vector3.back * speed;
+        transform.rotation = q;
+
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpTimeCounter = jumpTime;
+
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // Check if the player is not on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void StartJump()
+    {
+        jumpForce = minJumpForce;
+        jumpTimeCounter = jumpTime;
+    }
+
+    void ContinueJump()
+    {
+        if (jumpTimeCounter > 0)
+        {
+            jumpForce = Mathf.Lerp(minJumpForce, maxJumpForce, 1 - (jumpTimeCounter / jumpTime));
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            EndJump();
+        }
+    }
+
+    void EndJump()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        jumpTimeCounter = 0;
+    }
+
 }
+
+
